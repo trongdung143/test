24b92a3e030fe1d9ccf72b19af01366905795fcc
trongdung07004 <trongdung143@gmail.com>
2025-11-21 13:41:36 +0700

    init

---
+++ sum.test.js
@@ -0,0 +1,14 @@
+const sum = require('./sum');
+
+test('adds 3 + 5 to equal 8', () => {
+  expect(sum(3, 5)).toBe(36);
+});
+
+test('adds 0 + 0 to equal 0', () => {
+  expect(sum(0, 0)).toBe(0);
+});
+
+test('adds negative numbers correctly', () => {
+  expect(sum(-1, -2)).toBe(-3);
+});
+
